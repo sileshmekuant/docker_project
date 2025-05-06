4e2f7c0415e1204f2dd6015f23b149e9dae25005
Silesh Mekuant <sileshmekuant@gmail.com>
2025-05-06 08:16:04 +0300

    task4

--- purchase_m/__manifest__.py
+++ purchase_m/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','purchase','mail'],
+    'depends': ['base','purchase','mail','hr'],
 
     # always loaded
     'data': [
--- purchase_m/models/models.py
+++ purchase_m/models/models.py
@@ -3,12 +3,29 @@ from odoo import models, api, fields
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-   
-
     def action_send_email(self):
-        # Replace with your actual template's external ID
-        template = self.env.ref('purchase_m.email_template_purchase_request')
+        sender_email = self.env.user.company_id.email
 
-        for record in self:
-            if template:
-                template.send_mail(record.id, force_send=True)
+        # Use hardcoded recipient or fetch dynamically
+        recipient_email = "sileshmekuant@gmail.com"
+
+        # Fetch email template
+        template = self.env.ref("purchase_m.email_template_purchase_request")
+
+        # You can pass any object here; using self[0] or a dummy employee if needed
+        context_obj = self[0] if self else self.env['purchase.order']
+
+        # Render email content
+        body_html = template.with_context(object=context_obj)._render_field('body_html', [template.id])[template.id]
+        subject = template.with_context(object=context_obj)._render_field('subject', [template.id])[template.id]
+
+        # Send email to the target address
+        mail_values = {
+            'email_from': sender_email,
+            'email_to': recipient_email,
+            'subject': subject,
+            'body_html': body_html,
+        }
+
+        mail = self.env['mail.mail'].create(mail_values)
+        mail.send()
