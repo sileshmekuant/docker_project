8dcac9a312f103d7b1a77867ee554197e39e34d4
Silesh Mekuant <sileshmekuant@gmail.com>
2025-05-24 10:21:41 +0300

    rm assetmana

--- asset_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# from . import asset
-# from . import asset_category
-# from . import hr_employee
--- asset_management/models/asset_category.py
+++
@@ -1,7 +0,0 @@
-# from odoo import models, fields, api
-# class AssetCategory(models.Model):
-# 	_name = 'asset.category'
-# 	_description = 'Asset Category'
-
-# 	name = fields.Char(string="Category Name", required=True)
-
