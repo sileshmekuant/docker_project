30256c5af0eb5e26ca7238323839777cb1bef133
Silesh Mekuant <sileshmekuant@gmail.com>
2025-05-28 13:49:49 +0300

    action dashboard

--- asset/static/src/js/asset_dashboard.js
+++ asset/static/src/js/asset_dashboard.js
@@ -7,12 +7,15 @@ import { Component, onWillStart } from "@odoo/owl";
 export class AssetAssignmentDashboard extends Component {
     setup() {
         this.orm = useService("orm");
+        this.action = useService("action");
+
         this.state = {
             total: 0,
             assigned: 0,
             returned: 0,
             overdue: 0,
         };
+
         onWillStart(async () => {
             const result = await this.orm.call("asset.assignment", "get_dashboard_data", []);
             this.state.total = result.total;
@@ -21,8 +24,27 @@ export class AssetAssignmentDashboard extends Component {
             this.state.overdue = result.overdue;
         });
     }
+
+        openListView(type = null) {
+        const domainMap = {
+            assigned: [["state", "=", "assigned"]],
+            returned: [["state", "=", "returned"]],
+            overdue: [["state", "=", "overdue"]],
+        };
+        const domain = type ? domainMap[type] : [];
+
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Asset Assignments",
+            res_model: "asset.assignment",
+            view_mode: "list,form",
+            views: [[false, "list"], [false, "form"]],  // <-- required
+            domain: domain,
+            target: "current",
+        });
+    }
+
 }
 
 AssetAssignmentDashboard.template = "asset_assignment.Dashboard";
-
 registry.category("actions").add("asset_assignment_dashboard", AssetAssignmentDashboard);
